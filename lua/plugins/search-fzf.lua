e44acff99d26baffa53d9fe457c5f89dc1b5c744
eyebrowkang <48169104+eyebrowkang@users.noreply.github.com>
2023-06-08 22:19:40 +0800

    update fzf keymap

--- lua/plugins/search-fzf.lua
+++ lua/plugins/search-fzf.lua
@@ -15,10 +15,12 @@ return {
         },
       },
     })
+    vim.keymap.set('n', '<LEADER>fl', function() vim.cmd("FzfLua") end)
     vim.keymap.set('n', '<LEADER>ff', builtin.files, {})
-    vim.keymap.set('n', '<LEADER>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<LEADER>fg', builtin.live_grep_glob, {})
     vim.keymap.set('n', '<LEADER>fb', builtin.buffers, {})
     vim.keymap.set('n', '<LEADER>fh', builtin.help_tags, {})
-    vim.keymap.set('n', '<LEADER>fc', function() vim.cmd("FzfLua") end)
+    vim.keymap.set('n', '<LEADER>fp', builtin.lsp_finder, {})
+    vim.keymap.set('n', '<LEADER>fs', builtin.git_status, {})
   end,
 }
